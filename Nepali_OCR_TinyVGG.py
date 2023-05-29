7dc8ec09211e2da6c30fc6d38e6b6da4ff952f03
Pukar Karki <pukark7@gmail.com>
2023-05-29 17:15:53 +0545

    Add files via upload

---
+++ Nepali_OCR_TinyVGG.py
@@ -0,0 +1,75 @@
+import streamlit as st
+import pandas as pd
+import numpy as np
+import cv2
+from PIL import Image
+import torch
+import predict
+
+FILEPATH = 'models/model.pth'
+IMAGENAME = 'img.jpg'
+FONT_SIZE = 100
+
+if 'model' not in st.session_state:
+    st.session_state.model = predict.load_model(FILEPATH)
+
+if 'prediction' not in st.session_state:
+    st.session_state.prediction = predict.predict_on_image(IMAGENAME, st.session_state.model)
+
+st.set_page_config(
+    page_title="Nepali OCR",
+    page_icon="🇳🇵",
+    layout="wide",
+)
+
+
+st.title("Nepali Character Recognition")
+
+st.sidebar.markdown('# Nepali OCR')
+
+uploaded_file = st.file_uploader(label="Upload your image file",
+                 type=['jpg', 'jpeg'],
+                 accept_multiple_files=False,
+                 key="uploaded_file",
+                 on_change=None,
+                 args=None,
+                 kwargs=None,
+                 disabled=False,
+                 label_visibility="visible"
+)
+
+
+col1, col2 = st.columns(2)
+
+with col1:
+    st.write("Image")
+    if uploaded_file is not None:
+        with open(IMAGENAME,'wb+') as f:
+            f.write(uploaded_file.read())
+        
+    st.image(IMAGENAME, caption="Input Image", width=100)
+    
+
+with col2:
+    st.write("Predicted Character")
+    
+    html_str = f"""
+    <style>
+    .big-font{{
+    font: bold {FONT_SIZE}px Courier;
+    }}
+    </style>
+    <p class="big-font">{st.session_state.prediction}</p>
+    """
+
+    st.markdown(html_str, unsafe_allow_html=True)
+
+def perform_prediction():
+    st.session_state.prediction = predict.predict_on_image(IMAGENAME, st.session_state.model)
+
+st.button(label="Perform OCR", key="btn", on_click=perform_prediction)
+
+
+
+
+
---
+++ model_builder.py
@@ -0,0 +1,40 @@
+import torch
+from torch import nn
+
+# Setup device-agnostic code
+device = "cuda" if torch.cuda.is_available() else "cpu"
+
+class TinyVGG(nn.Module):
+    def __init__(self, input_shape: int, hidden_units: int, output_shape: int) -> None:
+        super().__init__()
+        self.conv_block_1 = nn.Sequential(
+            nn.Conv2d(in_channels=input_shape, 
+                      out_channels=hidden_units, 
+                      kernel_size=3, # how big is the square that's going over the image?
+                      stride=1, # default
+                      padding=1), # options = "valid" (no padding) or "same" (output has same shape as input) or int for specific number 
+            nn.ReLU(),
+            nn.Conv2d(in_channels=hidden_units, 
+                      out_channels=hidden_units,
+                      kernel_size=3,
+                      stride=1,
+                      padding=1),
+            nn.ReLU(),
+            nn.MaxPool2d(kernel_size=2,
+                         stride=2) # default stride value is same as kernel_size
+        )
+        self.conv_block_2 = nn.Sequential(
+            nn.Conv2d(hidden_units, hidden_units, kernel_size=3, padding=1),
+            nn.ReLU(),
+            nn.Conv2d(hidden_units, hidden_units, kernel_size=3, padding=1),
+            nn.ReLU(),
+            nn.MaxPool2d(2)
+        )
+        self.classifier = nn.Sequential(
+            nn.Flatten(),
+            nn.Linear(in_features=hidden_units*7*7,
+                      out_features=output_shape)
+        )
+    
+    def forward(self, x: torch.Tensor):
+        return self.classifier(self.conv_block_2(self.conv_block_1(x))) # <- leverage the benefits of operator fusion
